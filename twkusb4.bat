402093afc0b1d6db5b734b952a00e464bd68e0a9
IanDeRomania <62939108+IanDeRomania@users.noreply.github.com>
2024-01-21 09:30:46 -0800

    Add files via upload

---
+++ twkusb4.bat
@@ -0,0 +1,8 @@
+@echo off
+for /f %%i in ('wmic path Win32_USBController get PNPDeviceID') do set "str=%%i" & (
+echo.DEL USB Device Priority
+Reg.exe add "HKLM\System\CurrentControlSet\Enum\%%i\Device Parameters\Interrupt Management\Affinity Policy" /v "DevicePriority" /f
+echo.Enable MSI Mode on USB
+Reg.exe add "HKLM\System\CurrentControlSet\Enum\%%i\Device Parameters\Interrupt Management\MessageSignaledInterruptProperties" /v "MSISupported" /t REG_DWORD /d "1" /f
+)
+exit
