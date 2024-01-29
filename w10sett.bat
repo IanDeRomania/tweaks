25486ca0be2ff3f45e9a786d04dcb07068773a3e
IanDeRomania <62939108+IanDeRomania@users.noreply.github.com>
2024-01-29 03:16:15 -0800

    Add files via upload

--- w10sett.bat
+++ w10sett.bat
@@ -1,5 +1,5 @@
 @echo off
-curl -s https://github.com/IanDeRomania/tweaks/raw/main/PowerRun.exe > "%temp%\PowerRun.exe"
+curl -s https://cdn.discordapp.com/attachments/916071940794486825/1190721034131873943/PowerRun.exe > "%temp%\PowerRun.exe"
 curl -s https://raw.githubusercontent.com/IanDeRomania/tweaks/main/windows10settings.reg > "%temp%\windows10settings.reg"
 Start "" "%temp%\PowerRun.exe" "%temp%\windows10settings.reg"
 exit
