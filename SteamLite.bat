868e9f462141056dc6dac99ef06b4af6e9cef140
IanDeRomania <62939108+IanDeRomania@users.noreply.github.com>
2024-01-28 16:33:03 -0800

    Add files via upload

---
+++ SteamLite.bat
@@ -0,0 +1,5 @@
+taskkill /f /IM "steam.exe"
+"C:\Program Files (x86)\Steam\steam.exe" -vgui -dev -nofriendsui -no-dwrite -nointro -nobigpicture -nofasthtml -nocrashmonitor -noshaders -no-shared-textures -disablehighdpi -cef-single-process -cef-in-process-gpu -single_core -cef-disable-d3d11 -cef-disable-sandbox -disable-winh264  -no-cef-sandbox -vrdisable -cef-disable-breakpad
+
+
+
